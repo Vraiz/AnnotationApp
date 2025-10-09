278ecd33b02b405beed3f73d2d95adb3cc4f4717
ReythemGespenst <roncajumban@gmail.com>
2025-10-10 04:59:23 +0800

    feat: added global header that is accessible to all pages

---
+++ app/components/Globalheader.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const globalHeader = () => {
+    return (
+        <div id = "global_header">
+            Home Login Logout Account
+        </div>
+    )
+}
+
+export default globalHeader
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import GlobalHeader from './components/Globalheader';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GlobalHeader />
         {children}
       </body>
     </html>
