d7bf3a048aad6cc79e1a4525a706ef43a38d4381
ReythemGespenst <roncajumban@gmail.com>
2025-10-19 17:17:22 +0800

    feat: added css to globalheader and login page

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -57,7 +57,7 @@ const Login = () => {
                 <h1>Log in</h1>
                 <div id='quiz_div'><h3>Email: </h3><input type="email" name='email' placeholder='example.com' value={email} onChange={(e) => setEmail(e.target.value)}/></div>
                 <div id='quiz_div'><h3>Password: </h3><input type="password" name='password' placeholder='' value={password} onChange={(e) => setPassword(e.target.value)}/></div>
-                <button id='quiz_button' onClick={finishQuiz}>Register</button>
+                <button id='quiz_button' onClick={finishQuiz}>Register</button><br/>
                 <Link id='quiz_link' href="/Register">Don't have an account yet? Register here!</Link>
         </div>
         </div>
--- app/components/Globalheader.tsx
+++ app/components/Globalheader.tsx
@@ -1,10 +1,57 @@
 'use client'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from "next/navigation";
 
 const globalHeader = () => {
+    const router = useRouter();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState('');
+
+    useEffect(() => {
+        const userID = localStorage.getItem('userID');
+        if(userID) {
+            setIsLoggedIn(true);
+            fetchUser(userID);
+        }
+    }, [])
+
+    async function fetchUser(userID: string) {
+        try {
+            const res = await fetch(`/api/user?id=${userID}`);
+            const data = await res.json();
+            if (data.users) {
+                setUsername(data.users.username || 'User');
+        }
+        } catch (err) {
+            console.error('Error fetching user:', err);
+        }
+    }
+
+    const handleLogout = () => {
+        localStorage.clear();
+        setIsLoggedIn(false);
+        setUsername('');
+        router.push('/');
+    };
+
+    const handleLogin = () => {
+        router.push('/Login');
+    }
+
     return (
         <div id = "global_header">
-            Home Login Logout Account
+            <div id = "header_subset">
+                <div>
+                    <a href = "/">Home</a>
+                </div> 
+                <div>
+                    {isLoggedIn ? (
+                        <button onClick={handleLogout}>Logout</button>
+                    ) : (
+                        <button onClick={handleLogin}>Login</button>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
